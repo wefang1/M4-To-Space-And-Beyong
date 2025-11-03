5a15842d0cff0f2500011318673a07b630e79961
wefang1 <wefang1@s.sfusd.edu>
2025-11-03 10:42:14 -0800

    M4L3

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,7 +1,20 @@
 extends Sprite2D
 
-var velocity := Vector2(480, -480)
+var max_speed := 600.0
+var velocity := Vector2(0, 0)
+
 
 func _process(delta: float) -> void:
+	var direction := Vector2(0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	# If we don't do this and the player moves diagonally, they will move 40%
+	# faster than normal.
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	
+	velocity = direction * max_speed
 	position += velocity * delta
-	rotation = velocity.angle()
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
