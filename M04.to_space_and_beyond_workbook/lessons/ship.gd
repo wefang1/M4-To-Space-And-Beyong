87db3baa4e7b3b9d44e567c4aa9fa80bd4d1d391
wefang1 <wefang1@s.sfusd.edu>
2025-11-04 11:48:38 -0800

    M4L4 complete

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 
-var max_speed := 600.0
+var boost_speed := 1500.0
+var normal_speed := 600.0
+var max_speed := normal_speed
 var velocity := Vector2(0, 0)
 
 
@@ -14,7 +16,15 @@ func _process(delta: float) -> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
+	
 	velocity = direction * max_speed
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
