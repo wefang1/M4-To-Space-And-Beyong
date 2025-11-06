5bc09386d7fc605a0d650a7c0fe7b20328725893
wefang1 <wefang1@s.sfusd.edu>
2025-11-06 12:03:28 -0800

    M4L5 and M4L6 complete

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -4,7 +4,7 @@ var boost_speed := 1500.0
 var normal_speed := 600.0
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
-
+var steering_factor := 10.0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
@@ -20,11 +20,12 @@ func _process(delta: float) -> void:
 		max_speed = boost_speed
 		get_node("Timer").start()
 	
-	velocity = direction * max_speed
+	var desired_speed := max_speed * direction
+	var steering_vector := desired_speed - velocity
+	velocity += steering_factor * steering_vector * delta
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
-
 
 func _on_timer_timeout() -> void:
 	max_speed = normal_speed
